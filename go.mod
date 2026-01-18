2317ef4e2710aa1e5d84854e8bd8d72c43357dea
Thomas Davis <thomas.medavis@gmail.com>
2026-01-17 23:46:27 -0700

    Initial commit: Project structure and module setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thomazdavis/flashdb
+
+go 1.25.5
+
+require github.com/stretchr/testify v1.11.1 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("FlashDB is starting...")
+	fmt.Println("Initializing Memory Buffer... [Pending]")
+	fmt.Println("Checking Disk Logs... [Pending]")
+}
